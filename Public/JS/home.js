d7c0a915e52cedcb61fe1ae093e0ddd629ff2f3d
comcod <netosilvaneto6@gmail.com>
2024-10-19 14:02:05 -0300

    Ajuste de responsividade

--- Public/JS/home.js
+++ Public/JS/home.js
@@ -1,8 +1,7 @@
-// Seleciona o botão de menu e o container de links
+// Menu hamburguer
 const hamburgerMenu = document.getElementById('hamburger-menu');
 const navLinks = document.querySelector('.nav-links');
 
-// Adiciona o evento de clique para abrir ou fechar o menu
 hamburgerMenu.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });

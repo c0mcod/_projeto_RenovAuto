b2130982b7df4985732486cd5781ea3585ba85ee
comcod <netosilvaneto6@gmail.com>
2024-10-24 22:46:37 -0300

    ajustes de efeitos, teste de estilização do calendário da função de alugar e páginas de planos

--- Public/JS/reveal.js
+++ Public/JS/reveal.js
@@ -3,77 +3,93 @@ window.revelar = ScrollReveal({ reset: true });
 // Hero
 revelar.reveal('.text',
     {
-        duration: 2000,
+        duration: 1000,
         distance: '120px',
         origin: 'top'
     })
 revelar.reveal('#text-hero',
     {
-        duration: 2000,
+        duration: 1000,
         delay: 200,
         distance: '120px'
     })
-    revelar.reveal('.hero-btn',
-        {
-            duration: 2000,
-            distance: '120px',
-            delay: 300,
-            origin: 'bottom'
-        })
+revelar.reveal('.hero-btn',
+    {
+        duration: 1000,
+        distance: '120px',
+        delay: 300,
+        origin: 'bottom'
+    })
 
 // serviços
 revelar.reveal('.effect-reveal-1',
     {
-        duration: 2000,
-        distance: '120px',
+        duration: 1000,
+        distance: '100px',
         delay: 400,
-        origin: "bottom"
+        origin: "bottom",
+        easing: 'ease-in-out',
+        reset: true,
+        beforeReveal: function (el) {
+            el.style.transform = '';
+        }
     })
 
 
 revelar.reveal('.effect-reveal-2',
     {
-        duration: 2000,
-        distance: '120px',
+        duration: 1000,
+        distance: '100px',
         delay: 900,
-        origin: "bottom"
+        origin: "bottom",
+        easing: 'ease-in-out',
+        reset: true,
+        beforeReveal: function (el) {
+            el.style.transform = '';
+        }
     })
 
 
 revelar.reveal('.effect-reveal-3',
     {
-        duration: 2000,
-        distance: '120px',
+        duration: 1000,
+        distance: '100px',
         delay: 1500,
-        origin: "bottom"
+        origin: "bottom",
+
+        easing: 'ease-in-out',
+        reset: true,
+        beforeReveal: function (el) {
+            el.style.transform = '';
+        }
     })
 
 // planos 
 revelar.reveal('.content-services1',
     {
-        duration: 2000,
+        duration: 1000,
         distance: '120px',
         origin: 'left'
 
     })
 revelar.reveal('.content-services2',
     {
-        duration: 2000,
+        duration: 1000,
         distance: '120px',
         origin: 'right'
 
     })
 revelar.reveal('.title-plans',
     {
-        duration: 2000,
+        duration: 1000,
         distance: '120px'
     })
 
 revelar.reveal('.img-plan',
     {
-        duration: 2000,
+        duration: 1000,
         distance: '120px',
-        origin:"left"
+        origin: "left"
     })
 
 
